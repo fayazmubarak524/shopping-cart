1b4af4a66c7912e06a945db359ef4522b25a5dea
fayazmubarak524 <fayazmubarak524@gmail.com>
2023-01-15 22:28:28 +0530

    Add to cart

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const { response } = require("express");
 var express = require("express");
+const session = require("express-session");
 var router = express.Router();
 const productHelpers = require("../helpers/product-helpers");
 const userHelpers = require("../helpers/user-helpers");
@@ -12,11 +13,15 @@ const verifyLogin = (req, res, next) => {
 };
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", async (req, res, next) => {
   let user = req.session.user;
   console.log(user);
+  let cartCount = null;
+  if (req.session.user) {
+    cartCount = await userHelpers.getCartCount(req.session.user._id);
+  }
   productHelpers.getAllProducts().then((products) => {
-    res.render("user/view-products", { products, user });
+    res.render("user/view-products", { products, user, cartCount });
   });
 });
 router.get("/login", (req, res) => {
@@ -54,14 +59,15 @@ router.get("/logout", (req, res) => {
   req.session.destroy();
   res.redirect("/");
 });
-router.get("/cart", verifyLogin, async(req, res) => {
-  let products =await userHelpers.getCartProducts(req.session.user._id);
+router.get("/cart", verifyLogin, async (req, res) => {
+  let products = await userHelpers.getCartProducts(req.session.user._id);
   console.log(products);
-  res.render("user/cart");
+  res.render("user/cart", { products, user: req.session.user});
 });
-router.get("/add-to-cart/:id", verifyLogin, (req, res) => {
+router.get("/add-to-cart/:id", (req, res) => {
+  console.log("api call");
   userHelpers.addToCart(req.params.id, req.session.user._id).then(() => {
-    res.redirect("/");
+    res.json({ status: true });
   });
 });
 
